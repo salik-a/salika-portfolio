ec10c58c0550ff985b633d3dd5cffdc94b8e29fb
salik-a <salika@mef.edu.tr>
2024-06-29 16:27:12 +0300

    projects part done

---
+++ salika/src/components/Projects/ProjectCard.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import styles from "./ProjectCard.module.css";
+import { getImageUrl } from "../../utils";
+export type TProjectCard = {
+  project: {
+    title: string;
+    imageSrc: string;
+    description: string;
+    skills: [string];
+    sources: [string];
+    code: string;
+    source: string;
+  };
+};
+
+export const ProjectCard: React.FC<TProjectCard> = ({
+  project: { title, imageSrc, description, skills, code, sources },
+}) => {
+  return (
+    <div className={styles.container}>
+      <img
+        src={getImageUrl(imageSrc)}
+        alt={`Image of ${title}`}
+        className={styles.image}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li key={id} className={styles.skill}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.links}>
+        {code && (
+          <a href={code} className={styles.link}>
+            Github
+          </a>
+        )}
+        {sources.map((source, id) => {
+          return (
+            <a key={id} href={source} className={styles.link}>
+              {id === 0 ? "Play Store" : "App Store"}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
