8812b6eb262a4321f075b0326dc7e46b8028c8ca
salik-a <salika@mef.edu.tr>
2024-06-29 16:28:51 +0300

    components exported and added to app.tsx file

--- salika/src/App.tsx
+++ salika/src/App.tsx
@@ -1,7 +1,24 @@
 import styles from "./App.module.css";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Projects,
+} from "./components";
 
 function App() {
-  return <div className={styles.App}>Hello world</div>;
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
---
+++ salika/src/components/index.ts
@@ -0,0 +1,8 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+
+export { About, Contact, Experience, Hero, Navbar, Projects };
