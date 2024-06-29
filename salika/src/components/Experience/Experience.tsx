0ef0c42c853508ab14a46135906bb20bf1cdad8a
salik-a <salika@mef.edu.tr>
2024-06-29 16:28:03 +0300

    experience part done

---
+++ salika/src/components/Experience/Experience.tsx
@@ -0,0 +1,49 @@
+import styles from "./Experience.module.css";
+import skills from "./skills.json";
+import history from "./history.json";
+import { getImageUrl } from "../../utils";
+
+const Experience = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience & Skills</h2>
+      <div className={styles.content}>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <img
+                  src={getImageUrl(historyItem.imageSrc)}
+                  alt={`${historyItem.organisation} Logo`}
+                />
+                <div className={styles.historyItemDetails}>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
